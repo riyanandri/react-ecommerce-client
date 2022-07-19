85944f00fa39ffe8e05d6566f2ac13850f61478c
riyanandri <hiddenfoxe01@gmail.com>
2022-07-20 00:14:48 +0700

    added product detail page

---
+++ src/pages/Product.jsx
@@ -0,0 +1,167 @@
+import React from 'react'
+import styled from 'styled-components'
+import Navbar from '../components/Navbar';
+import Announcement from '../components/Announcement';
+import Newsletter from '../components/Newsletter';
+import Footer from '../components/Footer';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+
+const Container = styled.div``;
+
+const Wrapper = styled.div`
+    padding: 50px;
+    display: flex;
+`;
+
+const ImgContainer = styled.div`
+    flex: 1;
+`;
+
+const Image = styled.img`
+    width: 100%;
+    height: 90vh;
+    object-fit: cover;
+`;
+
+const InfoContainer = styled.div`
+    flex: 1;
+    padding: 0px 50px;
+`;
+
+const Title = styled.h1`
+    font-weight: 200;
+`;
+
+const Desc = styled.p`
+    margin: 20px 0px;
+`;
+
+const Price = styled.span`
+    font-weight: 100;
+    font-size: 40px;
+`;
+
+const FilterContainer = styled.div`
+    width: 50%;
+    margin: 30px 0px;
+    display: flex;
+    justify-content: space-between;
+`;
+
+const Filter = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const FilterTitle = styled.span`
+    font-size: 20px;
+    font-weight: 200;
+`;
+
+const FilterJenis = styled.select`
+    margin-left: 10px;
+    padding: 5px;
+`;
+
+const FilterJenisOption = styled.option``;
+
+const FilterUmur = styled.select`
+    margin-left: 10px;
+    padding: 5px;
+`;
+
+const FilterUmurOption = styled.option``;
+
+const AddContainer = styled.div`
+    width: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+const AmountContainer = styled.div`
+    display: flex;
+    align-items: center;
+    font-weight: 700;
+`;
+
+const Amount = styled.span`
+    width: 30px;
+    height: 30px;
+    border-radius: 10px;
+    border: 1px solid teal;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0px 5px;
+`;
+
+const Button = styled.button`
+    padding: 15px;
+    border: 2px solid teal;
+    background-color: white;
+    cursor: pointer;
+    font-weight: 600;
+
+    &:hover{
+        background-color: #f8f4f4;
+    }
+`;
+
+const Product = () => {
+  return (
+    <Container>
+        <Navbar/>
+        <Announcement/>
+        <Wrapper>
+            <ImgContainer>
+                <Image src="https://i.ibb.co/B4RN8XW/kategori2.jpg" />
+            </ImgContainer>
+            <InfoContainer>
+                <Title>Durian Montong</Title>
+                <Desc>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                    venenatis, dolor in finibus malesuada, lectus ipsum porta nunc, at
+                    iaculis arcu nisi sed mauris. Nulla fermentum vestibulum ex, eget
+                    tristique tortor pretium ut. Curabitur elit justo, consequat id
+                    condimentum ac, volutpat ornare.
+                </Desc>
+                <Price>Rp 75000</Price>
+                <FilterContainer>
+                    <Filter>
+                        <FilterTitle>Jenis</FilterTitle>
+                        <FilterJenis>
+                            <FilterJenisOption>Musang King</FilterJenisOption>
+                            <FilterJenisOption>Montong</FilterJenisOption>
+                            <FilterJenisOption>Bawor</FilterJenisOption>
+                        </FilterJenis>
+                    </Filter>
+                    <Filter>
+                        <FilterTitle>Umur</FilterTitle>
+                        <FilterUmur>
+                            <FilterUmurOption>1 Bulan</FilterUmurOption>
+                            <FilterUmurOption>2 Bulan</FilterUmurOption>
+                            <FilterUmurOption>3 Bulan</FilterUmurOption>
+                            <FilterUmurOption>6 Bulan</FilterUmurOption>
+                            <FilterUmurOption>12 Bulan</FilterUmurOption>
+                        </FilterUmur>
+                    </Filter>
+                </FilterContainer>
+                <AddContainer>
+                    <AmountContainer>
+                        <RemoveIcon/>
+                        <Amount>1</Amount>
+                        <AddIcon/>
+                    </AmountContainer>
+                    <Button>TAMBAH KE KERANJANG</Button>
+                </AddContainer>
+            </InfoContainer>
+        </Wrapper>
+        <Newsletter/>
+        <Footer/>
+    </Container>
+  )
+}
+
+export default Product
